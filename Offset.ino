b5d0771e2a3926edb911e272dc20a66acee0481b
fjbasile <fjbasile@icloud.com>
2021-05-31 13:42:24 -0500

    Update Offset.ino

--- Offset.ino
+++ Offset.ino
@@ -14,7 +14,7 @@ int delayTime = 2000;
 int offsetTime = 250;
 int pulseWidth = 500;
 
-volatile bool stopPulseTriggered;
+volatile bool stopPulseEnabled;
 
 int startPin = 2;
 int stopPin = 3;
@@ -25,7 +25,7 @@ void setup()
   pinMode(stopPin, OUTPUT);
   CircuitPlayground.begin();
   CircuitPlayground.strip.begin();\
-  stopPulseTriggered = false;
+  stopPulseEnabled = false;
   //Serial.begin(9600); //for debugging
 }
 
@@ -43,17 +43,17 @@ void loop()
   {
     tempTime = currentTime; //log the time at which the start pulse triggered high
     CircuitPlayground.setPixelColor(0, 0,127,0);
-    stopPulseTriggered = true;
+    stopPulseEnabled = true;
     digitalWrite(startPin, HIGH);
   }
 
   //if the stop pulse has been enabled and it has met the offset time requirements then it pulses high
-  if (stopPulseTriggered == true && currentTime - tempTime >= offsetTime)
+  if (stopPulseEnabled == true && currentTime - tempTime >= offsetTime)
   {   
     stopPulseHighTime = currentTime; //log the time of the stop pulse going high
     digitalWrite(stopPin, HIGH);
     CircuitPlayground.setPixelColor(9, 0, 0, 127);
-    stopPulseTriggered = false; //disable the stop pulse so we don't inadvertently re-enter this condition
+    stopPulseEnabled = false; //disable the stop pulse so we don't inadvertently re-enter this condition
   }
 
   //tempTime was updated during the start pulse firing. When it's on longer than pulse width, it goes low
@@ -64,7 +64,7 @@ void loop()
   }
 
   //if the stop pulse is deactivated and it has pulsed higher for the right duration, stop pulse goes low
-  if(stopPulseTriggered == false && currentTime - stopPulseHighTime >= pulseWidth)
+  if(stopPulseEnabled == false && currentTime - stopPulseHighTime >= pulseWidth)
   {
     CircuitPlayground.setPixelColor(9, 0, 0, 0);
     digitalWrite(stopPin, LOW);
